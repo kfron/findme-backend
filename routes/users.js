46cfd8c005ea9b3cc0803c663dc039535e9a355a
kfron <jakubfron99@gmail.com>
2021-12-21 13:56:18 +0100

    Rearrange responses and add a corner case

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,11 @@ const { bcrypt } = require('../services/auth');
 const router = express.Router();
 const users = require('../services/users');
 
-router.post('/login', async function(req, res, next) {
+router.post('/login', async function (req, res, next) {
   let email = req.body.email;
   let password = req.body.password;
   let user = null
-  
+
   try {
     user = (await users.getUser(email))[0];
   } catch (err) {
@@ -16,31 +16,36 @@ router.post('/login', async function(req, res, next) {
   }
 
   if (!user) {
-    res.status(401).json({'error': 'Incorrect email'})
+    res.json({ 'error': 'Incorrect email' })
   } else {
     bcrypt.compare(password, user.password, function (err, result) {
       if (result == true) {
-        res.status(200).json({email: user.email, password: password, is_admin: user.is_admin})
+        res.json({ email: user.email, password: password, is_admin: user.is_admin })
       } else {
-        res.status(401).json({'error': 'Incorrect password'})
+        res.json({ 'error': 'Incorrect password' })
       }
     })
   }
 });
 
-router.post('/signup', async function(req, res, next) {
+router.post('/signup', async function (req, res, next) {
   let email = req.body.email;
   let password = req.body.password;
   let is_admin = req.body.is_admin;
   let user = null;
 
-  try {
-    user = (await users.createUser(email, password, is_admin));
-  } catch (err) {
-    console.log("Error while creating user ", err.message);
-    next(err);
+  let test = await users.getUser(email)
+  if (test) {
+    res.json({ 'error': 'Email address taken' })
+  } else {
+    try {
+      user = (await users.createUser(email, password, is_admin));
+    } catch (err) {
+      console.log("Error while creating user", err.message);
+      next(err);
+    }
+    res.json(user);
   }
-  res.status(200).json(user);
 
 })
 
