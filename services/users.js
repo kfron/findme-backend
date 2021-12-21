3c944236f111e85a500fb18dcbf5f439ff5f06f3
kfron <jakubfron99@gmail.com>
2021-12-21 11:34:47 +0100

    Change API return value for user creation

--- services/users.js
+++ services/users.js
@@ -2,13 +2,19 @@ const db = require('./db');
 const auth = require('./auth');
 
 async function createUser(email, password, is_admin) {
+    let user = undefined;
     auth.bcrypt.hash(password, auth.saltRounds, function (err, hash) {
-        db
+        user = db
         .query(
-            'INSERT INTO users(email, password, created_at, is_admin) VALUES ($1, $2, now(), $3);', 
+            'INSERT INTO' +
+            'users(email, password, created_at, is_admin)' +
+            'VALUES ($1, $2, now(), $3)' +
+            'RETURNING email, password, created_at, is_admin;', 
             [email, hash, is_admin])
         .catch(err => console.error('Error executing query', err.stack))
     })
+
+    return user;
 }
 
 async function getUser(email) {
