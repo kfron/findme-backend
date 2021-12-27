90dd5f68ffeb3716601ff08c80983fd3606c3ea6
kfron <jakubfron99@gmail.com>
2021-12-27 10:32:21 +0100

    Feat: Add multer

--- routes/ads.js
+++ routes/ads.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const ads = require('../services/ads');
+const multer = require('multer');
+const { path } = require('../app');
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname))
+  }
+})
+const upload = multer({ storage: storage })
 
 router.get('/getAdsList', async function (req, res, next) {
   try {
@@ -21,23 +32,26 @@ router.get('/getAd', async function (req, res, next) {
   }
 });
 
-router.post('/createAd', async function (req, res, next) {
+router.post('/createAd', upload.single('image'), function (req, res, next) {
   console.log('something received');
-  let userId = req.body.userId;
+  console.log(req);
+  /*let userId = req.body.userId;
   let name = req.body.name;
   let age = req.body.age;
   let image = req.body.image;
   let description = req.body.description;
 
   console.log(req.body);
-
+*/
+  console.log(req.file);
+  console.log(req.body);
   let ad = null;
-  try {
+  /*try {
     ad = await ads.createAd(userId, name, age, image, description);
   } catch (err) {
     console.log('Error while creating ad ', err.message);
     next(err);
-  }
+  }*/
   res.json(ad);
 });
 
